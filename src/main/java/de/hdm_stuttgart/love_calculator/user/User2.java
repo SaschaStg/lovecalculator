809d4679a5ccfa23598c2877b2f759bf30439115
Simeon Schulz <ss534@hdm-stuttgart.de>
2021-05-29 17:16:37 +0200

    feat: added class User1 and User2 with Sascha

---
+++ src/main/java/de/hdm_stuttgart/love_calculator/user/User2.java
@@ -0,0 +1,10 @@
+package de.hdm_stuttgart.love_calculator.user;
+
+import java.util.ArrayList;
+
+public class User2 {
+
+    static final public ArrayList<String[]> result = new ArrayList<>();
+
+
+}
