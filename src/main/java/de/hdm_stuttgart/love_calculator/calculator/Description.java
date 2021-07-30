5d21ea8c1b0dab6ecc2f01ccd780656300b385b3
Vivien Volpert <vv014@hdm-stuttgart.de>
2021-07-30 16:23:24 +0200

    Changed text from descriptions.

--- src/main/java/de/hdm_stuttgart/love_calculator/calculator/Description.java
+++ src/main/java/de/hdm_stuttgart/love_calculator/calculator/Description.java
@@ -6,27 +6,49 @@ public class Description {
 
     public static String generateDescription(Session sessionDescription, int finalPercentage) {
         int percentage = finalPercentage;
-        if (percentage <= 10) {
-            return "Leider passt ihr nicht zusammen :(";
-        } else if (percentage > 10 && percentage <= 20) {
-            return "Naja.. das wird glaub nix.";
-        } else if (percentage > 20 && percentage <= 30) {
-            return "Eventuell könnt ihr ja Freunde sein..";
-        } else if (percentage > 30 && percentage <= 40) {
-            return "Also gute Freunde könntet ihr ja schon werden";
-        } else if (percentage > 40 && percentage <= 50) {
-            return "Könnte schwierig werden.. aber nicht unmöglich!";
-        } else if (percentage > 50 && percentage <= 60) {
-            return "Zwar keine Liebe auf den ersten Blick aber auch nicht hoffnungslos!";
-        } else if (percentage > 60 && percentage <= 70) {
-            return "Es könnte öfter Stress in der Beziehung geben aber ihr passt schon ein wenig zusammen!";
-        } else if (percentage > 70 && percentage <= 80) {
-            return "Zwischen euch funkt es.. da ist was!";
-        } else if (percentage > 80 && percentage <= 90) {
-            return "Seid ihr schon zusammen? Nein? Dann wirds höchste Zeit! Das passt super!";
-        } else if (percentage > 90 && percentage <= 100) {
-            return "Besser gehts nicht! Das nennt man Liebe auf den ersten Blick!";
-        }
+        if (percentage <= 4) {
+            return "Schon mal überlegt enthalten zu leben?";
+        } else if (percentage > 4 && percentage <= 9) {
+            return "Warum reimt sich dein Name auf Zölibat?";
+        } else if (percentage > 9 && percentage <= 14) {
+            return "Tausendmal berührt, tausendmal ist nichts passiert und dabei bleibt es auch!";
+        } else if (percentage > 14 && percentage <= 19) {
+            return "Im Leben bist du mal Windschutzscheibe und mal Insekt, mit dem Ergebnis wohl eher das Insekt.";
+        } else if (percentage > 19 && percentage <= 24) {
+            return "Satz mit x… das wird wohl nix,";
+        } else if (percentage > 24 && percentage <= 29) {
+            return "*Blütenblätter pflücken* liebt dich, liebt dich nicht, liebt dich, liebt dich NICHT!";
+        } else if (percentage > 29 && percentage <= 34) {
+            return "Verkauf dich nicht unter deinem Wert.";
+        } else if (percentage > 34 && percentage <= 39) {
+            return "Andere Eltern haben auch schöne Kinder.";
+        } else if (percentage > 39 && percentage <= 44) {
+            return "Lieber widerlich als wieder nicht.";
+        } else if (percentage > 44 && percentage <= 49) {
+            return "Einmal ist keinmal.";
+        } else if (percentage > 49 && percentage <= 54) {
+            return "Hm, ... irgendwie nichts Ganzes und nichts Halbes.";
+        } else if (percentage > 54 && percentage <= 59) {
+            return "Naja immerhin braucht eure Liebe keine weiteren Parteien für die absolute Mehrheit.";
+        } else if (percentage > 59 && percentage <= 64) {
+            return "Könnte besser sein, könnte schlechter sein, könnt's auch sein lassen.";
+        } else if (percentage > 64 && percentage <= 69) {
+            return "Könnte schwierig werden... aber nicht unmöglich!";
+        } else if (percentage > 69 && percentage <= 74) {
+            return "Besser wirds wohl nicht.";
+        } else if (percentage > 74 && percentage <= 79) {
+            return "Zwischen euch funkt es… da ist was!";
+        } else if (percentage > 79 && percentage <= 84) {
+            return "Auf gehts raus und Blumen kaufen!";
+        } else if (percentage > 84 && percentage <= 89) {
+            return "Eure Herzen schlagen im gleichen Takt.";
+        } else if (percentage > 89 && percentage <= 94) {
+            return "Ich höre schon die Hochzeitsglocken Leuten. Ab nach Vegas!";
+        } else if (percentage > 94 && percentage <= 99) {
+            return "Sehr geehrte Damen und Herren, darf ich Ihnen vorstellen? Das Traumpaar des Jahres!";
+        } else if (percentage == 100) {
+            return "Glaubst du an die große Liebe? Dann hast du sie gefunden – Glückwunsch!";
+
         return "Eure Liebe überfordert sogar das System.. das ist wohl was ganz besonderes?";
     }
 
