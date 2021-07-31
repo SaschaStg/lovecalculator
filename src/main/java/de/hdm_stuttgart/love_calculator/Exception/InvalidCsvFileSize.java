217dcd1ba3d5df044ee0ca65886ffd173cdc7633
Sascha Schmidt <ss532@hdm-stuttgart.de>
2021-07-31 17:56:22 +0200

    codewithme changes from simeon

--- src/main/java/de/hdm_stuttgart/love_calculator/Exception/InvalidCsvFileSize.java
+++ src/main/java/de/hdm_stuttgart/love_calculator/Exception/InvalidCsvFileSize.java
@@ -1,7 +1,15 @@
 package de.hdm_stuttgart.love_calculator.Exception;
 
+/**
+ * Handles invalid CSV file size
+ * Question Csv file and answer CSV file need the same amount of lines because every question object needs one answer object and vice versa
+ */
 public class InvalidCsvFileSize extends Exception {
 
+    /**
+     * Displays exception message
+     * @param message message which describes the exception
+     */
     public InvalidCsvFileSize(String message) {
         super(message);
     }
--- src/main/java/de/hdm_stuttgart/love_calculator/Game/Answers.java
+++ src/main/java/de/hdm_stuttgart/love_calculator/Game/Answers.java
@@ -4,16 +4,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 /**
- * Provides the answers
+ * Provides the answer objects
+ * Construct of an answer object
  */
 public class Answers {
-    // Input-Type: checkbox / radiobutton / textfield / slider
-    public final String INPUTTYPE;
+
     /**
-     * Stores the answer options
+     * Stores the input type of an answer object
+     * radiobutton, checkbox, textfield
+     */
+    public final String INPUTTYPE;
+
+    /**
+     * Stores the multiple answer options
      */
     private final List<String> answerOptions = new ArrayList<>();
 
+    /**
+     * Constructor of an answer object
+     * @param inputType the input type as a string
+     * @param answer the first answer as a string
+     * @param moreAnswers if a question has multiple answer options, they are stored in this string array
+     */
     public Answers(String inputType, final String answer, String[] moreAnswers){
         this.INPUTTYPE = inputType.trim().toUpperCase();
         this.answerOptions.add(answer);
@@ -21,10 +33,20 @@ public class Answers {
             this.answerOptions.addAll(Arrays.asList(moreAnswers));
     }
 
+
+    /**
+     * Informs you about the number of answers
+     * @return the size of list answerOptions
+     */
     public int getAnswersCount() {
         return answerOptions.size();
     }
 
+    /**
+     * Provides the "raw" answer as a string, based on an index
+     * @param answerIndex index of the required answer
+     * @return needed answer as a string
+     */
     public String getAnswer(int answerIndex) {
         return answerOptions.get(answerIndex);
     }
